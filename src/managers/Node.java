5ea8b4c746dbcd8827a71f23e34ccc741360489a
bendinini <arestova.d@bk.ru>
2024-04-15 16:39:06 +0300

    2: Fix access modifiers in class Node. Отправка 2

--- src/managers/Node.java
+++ src/managers/Node.java
@@ -5,9 +5,29 @@ import models.Task;
 public class Node {
     Task task;
     Node prev;
-    Node next;
+    protected Node next;
 
     public Node(Task task) {
         this.task = task;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
     }
 }
